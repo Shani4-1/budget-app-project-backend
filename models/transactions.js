e5fdf43935ae35bfbe0b3186c4e8d7ea968c83d1
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-04-18 11:33:13 -0400

    Added new key value pair to transactions array

--- models/transactions.js
+++ models/transactions.js
@@ -3,24 +3,28 @@ module.exports = [
         date: new Date("2023-02-15"),
         transactionDescription: "Internet",
         amount: 138.96,
+        deposit: false,
         from: "" ,
     },
     {
         date: new Date("2023-02-27"),
         transactionDescription: "Phone Bill",
         amount: 75.75,
+        deposit: false,
         from: "",
     },
     {
         date: new Date("2023-02-18"),
         transactionDescription: "Pay Day",
         amount: 3333.33,
+        deposit: true,
         from: "Solecost Inc.",
     },
     {
         date: new Date("2023-01-01"),
         transactionDescription: "Childcare",
         amount: 126.00,
+        deposit: false,
         from: "",
     },
 ]
