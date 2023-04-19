58d4ae205b1eb9ad9f0d8f7339d75ea2301b3206
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-04-19 07:55:53 -0400

    debugging route updates

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -16,48 +16,61 @@ transactions.get("/", (req, res) => {
         transaction.id = uuidv4();
       }
     });
+    
     res.send(transactionsArray)
   });
 
 //Create new transaction
 transactions.post("/", transactionsValidator, (req, res) => {
     const newTransaction = req.body;
-    newTransaction.id = uuidv4();
+    newTransaction.id = uuidv4(); // add the id property
     transactionsArray.push(newTransaction);
     res.status(201).json(newTransaction);
   });
+  
 
    
 
-//Get one transaction by index
-transactions.get("/:index", (req, res) => {
-    const { index} = req.params;
-    if (transactionsArray[index]) {
-        res.status(201).json(transactionsArray[index]);
+//Get one transaction by id
+transactions.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const transaction = transactionsArray.find((t) => t.id === id);
+    if (transaction) {
+      res.status(201).json(transaction);
     } else {
-        res.redirect(301, "/transactions");
+      res.status(404).json({ error: "Transaction not found" });
     }
-});
+  });
+  
 
 //Update transaction 
-transactions.put("/:index", transactionsValidator, (req, res) => {
-    const { index } = req.params;
+transactions.put("/:id", transactionsValidator, (req, res) => {
+    const { id } = req.params;
     const updatedTransaction = req.body;
-    updatedTransaction.id = transactionsArray[index].id;
-    transactionsArray[index] = updatedTransaction;
-    res.status(200).json(updatedTransaction);
+    updatedTransaction.id = id; // set the ID to the parameter value
+    const index = transactionsArray.findIndex((t) => t.id === id);
+    if (index !== -1) {
+      transactionsArray[index] = updatedTransaction;
+      res.status(200).json(updatedTransaction);
+    } else {
+      res.status(404).json({ error: "Transaction not found" });
+    }
   });
+  
+
 
 //Delete transaction
-transactions.delete("/:index", (req, res) => {
-    const { index } = req.params;
-    if (transactionsArray[index]) {
+transactions.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const index = transactionsArray.findIndex(transaction => transaction.id === id);
+    if (index !== -1) {
         const deletedTransaction = transactionsArray.splice(index, 1)[0];
         res.status(201).json(deletedTransaction);
     } else {
         res.status(404).json({error: "Page Not Found"});
     }
 });
+
 
 
 module.exports = transactions;
